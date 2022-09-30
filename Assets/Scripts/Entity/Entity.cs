9f199a735c591351f698447c02a57840278ead42
Vincent CLERC <Vi.CLERC1@formation-cci.fr>
2022-09-30 15:26:23 +0200

    following entity + player attaque

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+abstract public class Entity : MonoBehaviour
 {
 
-    public delegate void EntityEvent();
+
+    // Entity event
+    public delegate void EntityEvent(Entity e);
     public event EntityEvent OnDead;
 
+    // Entity movement
+    public GameObject Planet;
+    Vector3 Groundnormal;
+    bool OnGround = false;
+    float distanceToGround;
+    float gravity = 100;
+    protected Rigidbody rb;
+
+    // Entity stats 
     private float _life = 20f;
+    public float speed = 4;
+    public float jumpHeight = 1.2f;
     public float damage = 1.5f;
     public float attaqueDelay = 1f;
+    public float attaqueReload = 0f;
 
     public float Life {
         get => GetLife();
@@ -32,6 +46,27 @@ public class Entity : MonoBehaviour
         
     }
 
+    // ---------------------------------------------------------------
+    // Entity utils
+    // ---------------------------------------------------------------
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        EntityRotation();
+        EntityMovement();
+        Jump();
+        GroundControl();
+        GravityAndRotation();
+    }
+
     // get object position
     public Vector3 GetGameObjectPostition(GameObject o) {
 
@@ -44,14 +79,72 @@ public class Entity : MonoBehaviour
         return pos;
     }
 
+    // ---------------------------------------------------------------
+    // Entity moving
+    // ---------------------------------------------------------------
+
+    protected abstract void EntityMovement();
+
+    protected abstract void EntityRotation();
+
+    void Jump()
+    {
+        // Jump of the player
+        if (Input.GetKeyDown(KeyCode.Space) && OnGround == true)
+        {
+            rb.AddForce(transform.up * 40000 * jumpHeight * Time.deltaTime);
+
+        }
+    }
+
+    void GroundControl()
+    {
+        //GroundControl
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
+        {
+
+            distanceToGround = hit.distance;
+            Groundnormal = hit.normal;
+
+            if (distanceToGround <= 0.2f)
+            {
+                OnGround = true;
+                return;
+            }
+
+        }
+
+        OnGround = false;
+    }
+
+    void GravityAndRotation()
+    {
+        //GRAVITY and ROTATION
+        Vector3 gravDirection = (transform.position - Planet.transform.position).normalized;
+        if (OnGround == false)
+        {
+            rb.AddForce(gravDirection * -gravity);
+        }
+
+        // Rotation of the player to the planet
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, Groundnormal) * transform.rotation;
+        transform.rotation = toRotation;
+    }
+
+    // ---------------------------------------------------------------
+    // Entity actions
+    // ---------------------------------------------------------------
+
+    // attaque entity action
     public void Attaque(Entity e) {
         e.Life = e.Life - this.damage;
         this.attaqueDelay = 1;
-        Debug.Log(e.Life);
-    } 
+    }
 
+    // die action
     private void Die() {
-        this.OnDead.Invoke();
+        this.OnDead.Invoke(this);
         Destroy(this.gameObject);
     }
 
--- Assets/Scripts/Entity/IA_Entity.cs
+++ Assets/Scripts/Entity/IA_Entity.cs
@@ -4,70 +4,92 @@ using UnityEngine;
 
 public class IA_Entity : Entity
 {
-
-    public UnityEngine.AI.NavMeshAgent agent;
-
     public GameObject target;
     private Entity targetEntity;
+
     private bool targetIsDead = false;
 
     public float targetMinDistance = 1f;
+    public float targetMaxDistance = 30f;
 
     // Start is called before the first frame update
-    void Start()
+    protected new void Start()
     {
-        this.MoveToTarget();
-        targetEntity = target.GetComponent("Entity")  as Entity;
-        targetEntity.OnDead += TargetIsDead; 
+        /*this.MoveToTarget();*/
+        base.Start();
+        targetEntity = target.GetComponent("Entity") as Entity;
+        targetEntity.OnDead += TargetIsDead;
     }
 
     // Update is called once per frame
-    void Update() {
-        if(!targetIsDead) {
-            this.ManageTarget();
-        }
-    }
+    protected new void Update()
+    {
 
-    public void ManageTarget() {
-        float dist = Vector3.Distance(this.transform.position, target.transform.position);
-
-        // move entity when it is distant to taget
-        if(dist > this.targetMinDistance) {
-            this.MoveToTarget();
+        if(targetIsDead) {
+            return;
         }
 
-        this.attaqueDelay -= Time.deltaTime;
+        float dist = Vector3.Distance(GetGameObjectPostition(target), GetGameObjectPostition(this.gameObject));
 
-        // stop entity when it is next to target
-        if(dist < this.targetMinDistance) {
-            this.Stop();
+        // if out of range, nothing
+        if (dist > this.targetMaxDistance)
+        {
+            return;
+        }
+
+        // if to far, run to target
+        if(dist > this.targetMinDistance)
+        {
+            base.Update();
+        }
+
+        // if arround, attaque
+        if (dist < this.targetMinDistance)
+        {
             this.TryAttaque();
         }
+
     }
 
-    // move entity to taget
-    private void MoveToTarget() {
-        agent.SetDestination(
-            this.GetGameObjectPostition(this.target)
-        );
+    // move entity
+    protected override void EntityMovement()
+    {
+        Vector3 diff = GetGameObjectPostition(target) - GetGameObjectPostition(this.gameObject);
+        diff.Normalize();
+
+        float x = diff.x * speed * Time.deltaTime;
+        float y = diff.z * speed * Time.deltaTime;
+
+        transform.position += diff * speed * Time.deltaTime;
     }
 
-    // stop entity
-    private void Stop() {
-        agent.SetDestination(
-            this.GetGameObjectPostition(this.gameObject)
-        );
+    // Rotation to the target
+    protected override void EntityRotation()
+    {
+        Debug.Log("rotate");
+        this.transform.LookAt(target.transform);
+        this.transform.localRotation = Quaternion.Euler(0, this.transform.localRotation.eulerAngles.y, 0);
+        /*Vector2 lookDirection = new Vector2(GetGameObjectPostition(target).x, GetGameObjectPostition(target).z);
+        float angle = Mathf.Tan(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Euler(0, -angle, 0);*/
     }
 
     // try to inflict damage to the target 
-    private void TryAttaque() {
-        if(this.attaqueDelay < 0) {
+    private void TryAttaque()
+    {
+
+        this.attaqueReload -= Time.deltaTime;
+
+        if (this.attaqueReload < 0)
+        {
+            this.attaqueReload = this.attaqueDelay;
             this.Attaque(targetEntity);
         }
     }
 
     // set targetIsDead true to not update target anymore 
-    private void TargetIsDead() {
+    private void TargetIsDead(Entity e)
+    {
         targetIsDead = true;
     }
 
